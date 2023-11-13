95fc9ce7a914dbcdc44fb9a77e0c5cffd492de68
bug4tti16 <132424220+bug4tti16@users.noreply.github.com>
2023-11-13 12:23:46 +0900

    Update 모듈 업데이트.bat

--- Scripts/모듈 업데이트.bat
+++ Scripts/모듈 업데이트.bat
@@ -6,3 +6,7 @@ python3 -m pip install datetime
 python3 -m pip install multiprocessing
 python3 -m pip install jamo
 python3 -m pip install webbrowser
+python3 -m pip install customtkinter
+python3 -m pip install pandas
+python3 -m pip install openpyxl
+python3 -m pip install pyinput
