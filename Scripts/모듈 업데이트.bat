2f0de7fe3e5ad6bdeb6d68e11424d2bf2bd121ed
bug4tti16 <132424220+bug4tti16@users.noreply.github.com>
2023-12-14 16:26:20 +0900

    Update 모듈 업데이트.bat

--- Scripts/모듈 업데이트.bat
+++ Scripts/모듈 업데이트.bat
@@ -1,12 +1,13 @@
 @echo off
 python.exe -m pip install --upgrade pip
-python3 -m pip install pathlib
-python3 -m pip install pyserial
-python3 -m pip install datetime
-python3 -m pip install multiprocessing
-python3 -m pip install jamo
-python3 -m pip install webbrowser
-python3 -m pip install customtkinter
-python3 -m pip install pandas
-python3 -m pip install openpyxl
-python3 -m pip install pyinput
+pip install pathlib
+pip install pyserial
+pip install datetime
+pip install multiprocessing
+pip install jamo
+pip install webbrowser
+pip install customtkinter
+pip install pandas
+pip install openpyxl
+pip install pyinput
+pip install sqlite3
